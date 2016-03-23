9b29b6da6366cd7c9c1a6844a9dc0addfe3241d6
Steve Clay <steve@mrclay.org>
2016-03-23 14:17:06 -0400

    feat(changelog): integrate CHANGELOG header as release notes
    Between releases, commits may add release notes at the top of CHANGELOG.
    When we process the log, any content before the first anchor (`<a name="`)
    is moved between the new header and the contributors.

--- index.js
+++ index.js
@@ -13,16 +13,42 @@ function generate(options, done) {
     file: 'CHANGELOG.md',
     subtitle: '',
     log: console.log.bind(console),
+    notes: '',
   }, options || {});
 
   if (!options.version) {
     return done('No version specified');
   }
 
-  git.getTags(function(err, tags) {
-    if (err || !tags) return done('Failed to read git tags.\n'+err);
-    writeChangelog(tags);
+  readNotes(options.notes, options.file).then(function (notes) {
+    options.notes = notes;
+
+    git.getTags(function(err, tags) {
+      if (err || !tags) {
+        return done('Failed to read git tags.\n'+err);
+      }
+      writeChangelog(tags);
+    });
   });
+
+  // consider anything before first anchor as release notes
+  function readNotes(notes, file) {
+    return new Promise(function (resolve, reject) {
+      if (notes || !file || !fs.existsSync(file)) {
+        resolve(notes);
+        return;
+      }
+
+      fs.readFile(file, {encoding:'UTF-8'}, function(err, contents) {
+        if (err) {
+          return done('Failed to read ' + file + '.\n'+err);
+        }
+
+        var idx = contents.indexOf('<a name="');
+        resolve(idx ? contents.substr(0, idx) : '');
+      });
+    });
+  }
 
   function writeChangelog(tags) {
     options.exclude = options.exclude || tags;
@@ -31,11 +57,13 @@ function generate(options, done) {
     options.log('Generating changelog for %s...', options.version);
 
     git.getLog({
-      exclude: options.exclude, 
+      exclude: options.exclude,
       to: options.to,
     }, function(err, gitLog) {
-      if (err) return done('Failed to read git log.\n'+err);
-      
+      if (err) {
+        return done('Failed to read git log.\n'+err);
+      }
+
       writeGitLog(gitLog);
     });
   }
@@ -43,14 +71,22 @@ function generate(options, done) {
   function writeGitLog(gitLog) {
     options.log('Parsed %d commits.', gitLog.commits.length);
     options.log('Parsed %d contributors.', gitLog.contributors.length);
-    
+
     writer.writeLog(gitLog, options, function(err, changelog) {
-      if (err) return done('Failed to write changelog.\n'+err);
+      if (err) {
+        return done('Failed to write changelog.\n'+err);
+      }
 
       if (options.file && fs.existsSync(options.file)) {
         fs.readFile(options.file, {encoding:'UTF-8'}, function(err, contents) {
-          if (err) return done('Failed to read ' + options.file + '.\n'+err);
-          done(null, changelog + '\n' + String(contents));
+          if (err) {
+            return done('Failed to read ' + options.file + '.\n'+err);
+          }
+
+          // clear out release notes (already captured in log)
+          var idx = contents.indexOf('<a name="');
+
+          done(null, changelog + '\n' + String(contents.substring(idx)));
         });
       } else {
         done(null, changelog);
@@ -58,4 +94,3 @@ function generate(options, done) {
     });
   }
 }
-
